4fe645b2f076a5b6f8b6fddf08ed4f0963e3c3db
Abdelali <bahloul97abdel@gmail.com>
2019-10-21 20:41:56 +0100

    add the order model and import it in orders api

--- api/models/order.js
+++ api/models/order.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 
 const orderScheme = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId, //we use a propriete of mongosse to give for any order an id like 'ad5h5jd5jsl5jddk8kd'
-    name: { type: String, required: true },
-    price: { type: Number, required: true }
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantite: { type: Number, default: 1 }
 });
 
 module.exports = mongoose.model('Order', orderScheme); //we export that schema to use it in our app.js in the folder routes
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const Order = require('../models/order');
+
 router.get('/', (req, res, next) => {
 
     res.status(200).json({
