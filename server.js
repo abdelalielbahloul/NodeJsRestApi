435a10dcd39380c9d5194a016755c3bfe62f378d
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-03-13 11:57:10 +0100

    refactor : add node_env and show message where server is runing

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT;
+const environment = process.env.NODE_ENV;
 const server = http.createServer(app);
 
-server.listen(port)
+server.listen(port, console.log(
+    `\t-The server is running on ${environment} environment
+    \t-In localhost:${port}`
+))
